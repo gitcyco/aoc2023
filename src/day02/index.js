9b5c54ccbddf91b86f929e3c90e100a77f7e24cc
gitcyco <gitcyco@users.noreply.github.com>
2023-12-02 16:06:33 -0800

    moved repo

--- src/day02/index.js
+++ src/day02/index.js
@@ -10,7 +10,7 @@ const part1 = (rawInput) => {
     green: 13,
     blue: 14,
   };
-  const games = {};
+  // const games = {};
   let sum = 0;
   for (let game of lines) {
     let addGame = true;
@@ -43,11 +43,11 @@ run({
     tests: [
       {
         input: `
-        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
         `,
         expected: 8,
       },
@@ -56,10 +56,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+          Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+          Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+          Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+          Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+          Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        `,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },
