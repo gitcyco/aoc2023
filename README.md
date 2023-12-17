ed37f66caf4fc8680ed92edd01a12bfe7fa6969e
gitcyco <gitcyco@users.noreply.github.com>
2023-12-16 22:28:32 -0800

    day 16

--- README.md
+++ README.md
@@ -26,7 +26,7 @@
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%86/yellow)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
 [![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
-![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%85/green)](src/day16)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
@@ -167,16 +167,16 @@ Both parts: 0.979ms
 
 ```
 Day 15
-Time part 1: 1.782ms
-Time part 2: 198.372ms
-Both parts: 200.154ms
+Time part 1: 1.557ms
+Time part 2: 8.567ms
+Both parts: 10.124ms
 ```
 
 ```
 Day 16
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 7.454ms
+Time part 2: 362.548ms
+Both parts: 370.002ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 24/50
-Total time: 472.232ms
+Total stars: 26/50
+Total time: 652.204ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day16/index.js
@@ -0,0 +1,404 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  // console.log(input);
+  const lines = input.split("\n").map((e) => e.trim());
+  // console.log(lines);
+  const map = lines.map((e) =>
+    e.split("").map((item) => {
+      if (item === ".") return new Empty();
+      // else if (/\\/.test(item)) return new Mirror("Lmirror");
+      else if (item === "\\") return new Mirror("Lmirror");
+      else if (item === "/") return new Mirror("Rmirror");
+      else if (item === "|") return new Reflector("|");
+      else if (item === "-") return new Reflector("-");
+      else
+        throw new Error(
+          `Unknown node type: ${item}, code: ${item.charCodeAt(0)}`,
+        );
+    }),
+  );
+  // console.log(map);
+  let total = 0;
+  const [yLen, xLen] = [map.length, map[0].length];
+  const beams = [];
+  beams.push(new Beam([0, -1], "E"));
+
+  while (beams.length > 0) {
+    const beam = beams.pop();
+
+    beam.move();
+    const [curY, curX] = beam.current;
+    // If the beam is out of bounds, kill the beam
+    if (curY > yLen - 1 || curX > xLen - 1 || curY < 0 || curX < 0) {
+      // kill the beam by continuing the while loop
+      continue;
+    }
+    const currentNode = map[curY][curX];
+    // If any beam has entered this node from this direction before, kill the beam
+    if (currentNode.enteredFrom[beam.from]) {
+      // kill the beam by continuing the while loop
+      continue;
+    } else {
+      // otherwise mark that we entered from this direction before
+      currentNode.enteredFrom[beam.from] = true;
+    }
+    if (!currentNode.visited) {
+      total++;
+      currentNode.visited = true;
+    }
+
+    switch (currentNode.type) {
+      case "empty":
+        beams.push(beam);
+        break;
+      case "Lmirror":
+        beam.changeDir(currentNode.reflect(beam.from));
+        beams.push(beam);
+        break;
+      case "Rmirror":
+        beam.changeDir(currentNode.reflect(beam.from));
+        beams.push(beam);
+        break;
+      case "|":
+        if (beam.from in currentNode.split) {
+          let [dirA, dirB] = currentNode.split[beam.from];
+          beam.changeDir(dirA);
+          const newBeam = new Beam([curY, curX], dirB);
+          beams.push(beam);
+          beams.push(newBeam);
+        } else {
+          beams.push(beam);
+        }
+        break;
+      case "-":
+        if (beam.from in currentNode.split) {
+          let [dirA, dirB] = currentNode.split[beam.from];
+          beam.changeDir(dirA);
+          const newBeam = new Beam([curY, curX], dirB);
+          beams.push(beam);
+          beams.push(newBeam);
+        } else {
+          beams.push(beam);
+        }
+        break;
+    }
+
+    // console.log(
+    //   "beam is currently at:",
+    //   beam.current,
+    //   "which is:",
+    //   map[curY][curX],
+    // );
+  }
+  console.log("number of affected nodes:", total);
+  return total;
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const lines = input.split("\n").map((e) => e.trim());
+  const [yLen, xLen] = [lines.length, lines[0].length];
+  let max = -Infinity;
+
+  for (let y = 0; y < yLen; y++) {
+    if (y === 0) {
+      max = Math.max(max, processMap([0, -1], "E", lines));
+      max = Math.max(max, processMap([-1, 0], "S", lines));
+      max = Math.max(max, processMap([0, xLen], "W", lines));
+      max = Math.max(max, processMap([-1, xLen - 1], "S", lines));
+    } else if (y === yLen - 1) {
+      max = Math.max(max, processMap([yLen, 0], "N", lines));
+      max = Math.max(max, processMap([yLen - 1, -1], "E", lines));
+      max = Math.max(max, processMap([yLen - 1, xLen], "W", lines));
+      max = Math.max(max, processMap([yLen, xLen - 1], "N", lines));
+    }
+    max = Math.max(max, processMap([y, -1], "E", lines));
+    max = Math.max(max, processMap([y, xLen], "W", lines));
+  }
+  for (let x = 1; x < xLen - 1; x++) {
+    max = Math.max(max, processMap([-1, x], "S", lines));
+    max = Math.max(max, processMap([yLen, x], "N", lines));
+  }
+
+  // for (let y = 0; y < yLen; y++) {
+  //   for (let x = 0; x < xLen; x++) {
+  //     if (x === 0 && y === 0) {
+  //       max = Math.max(max, processMap([0, -1], "E", lines));
+  //       max = Math.max(max, processMap([-1, 0], "S", lines));
+  //     } else if (x === xLen - 1 && y === 0) {
+  //       max = Math.max(max, processMap([0, xLen], "W", lines));
+  //       max = Math.max(max, processMap([-1, xLen - 1], "S", lines));
+  //     } else if (x === xLen - 1 && y === yLen - 1) {
+  //       max = Math.max(max, processMap([yLen - 1, xLen], "W", lines));
+  //       max = Math.max(max, processMap([yLen, xLen - 1], "N", lines));
+  //     } else if (x === 0 && y === yLen - 1) {
+  //       max = Math.max(max, processMap([yLen - 1, -1], "E", lines));
+  //       max = Math.max(max, processMap([yLen, 0], "N", lines));
+  //     } else if (y === 0) {
+  //       max = Math.max(max, processMap([-1, x], "S", lines));
+  //     } else if (x === 0) {
+  //       max = Math.max(max, processMap([y, -1], "E", lines));
+  //     } else if (y === yLen - 1) {
+  //       max = Math.max(max, processMap([yLen - 1, x], "N", lines));
+  //     } else if (x === xLen - 1) {
+  //       max = Math.max(max, processMap([y, xLen], "W", lines));
+  //     }
+  //   }
+  // }
+  // let output = processMap([0, -1], "E", lines);
+  console.log("part 2 output:", max);
+  return max;
+};
+
+function processMap(start, startDir, lines) {
+  // const input = parseInput(rawInput);
+  // console.log(input);
+  // const lines = input.split("\n").map((e) => e.trim());
+  // console.log(lines);
+  const map = lines.map((e) =>
+    e.split("").map((item) => {
+      if (item === ".") return new Empty();
+      // else if (/\\/.test(item)) return new Mirror("Lmirror");
+      else if (item === "\\") return new Mirror("Lmirror");
+      else if (item === "/") return new Mirror("Rmirror");
+      else if (item === "|") return new Reflector("|");
+      else if (item === "-") return new Reflector("-");
+      else
+        throw new Error(
+          `Unknown node type: ${item}, code: ${item.charCodeAt(0)}`,
+        );
+    }),
+  );
+  // console.log(map);
+  let total = 0;
+  const [yLen, xLen] = [map.length, map[0].length];
+  const beams = [];
+  beams.push(new Beam(start, startDir));
+
+  while (beams.length > 0) {
+    const beam = beams.pop();
+
+    beam.move();
+    const [curY, curX] = beam.current;
+    // If the beam is out of bounds, kill the beam
+    if (curY > yLen - 1 || curX > xLen - 1 || curY < 0 || curX < 0) {
+      // kill the beam by continuing the while loop
+      continue;
+    }
+    const currentNode = map[curY][curX];
+    // If any beam has entered this node from this direction before, kill the beam
+    if (currentNode.enteredFrom[beam.from]) {
+      // kill the beam by continuing the while loop
+      continue;
+    } else {
+      // otherwise mark that we entered from this direction before
+      currentNode.enteredFrom[beam.from] = true;
+    }
+    if (!currentNode.visited) {
+      total++;
+      currentNode.visited = true;
+    }
+
+    switch (currentNode.type) {
+      case "empty":
+        beams.push(beam);
+        break;
+      case "Lmirror":
+        beam.changeDir(currentNode.reflect(beam.from));
+        beams.push(beam);
+        break;
+      case "Rmirror":
+        beam.changeDir(currentNode.reflect(beam.from));
+        beams.push(beam);
+        break;
+      case "|":
+        if (beam.from in currentNode.split) {
+          let [dirA, dirB] = currentNode.split[beam.from];
+          beam.changeDir(dirA);
+          const newBeam = new Beam([curY, curX], dirB);
+          beams.push(beam);
+          beams.push(newBeam);
+        } else {
+          beams.push(beam);
+        }
+        break;
+      case "-":
+        if (beam.from in currentNode.split) {
+          let [dirA, dirB] = currentNode.split[beam.from];
+          beam.changeDir(dirA);
+          const newBeam = new Beam([curY, curX], dirB);
+          beams.push(beam);
+          beams.push(newBeam);
+        } else {
+          beams.push(beam);
+        }
+        break;
+    }
+
+    // console.log(
+    //   "beam is currently at:",
+    //   beam.current,
+    //   "which is:",
+    //   map[curY][curX],
+    // );
+  }
+  // console.log("number of affected nodes:", total);
+  return total;
+}
+
+class Node {
+  constructor() {
+    this.enteredFrom = {
+      N: false,
+      S: false,
+      E: false,
+      W: false,
+    };
+    this.visited = false;
+  }
+}
+
+class Beam {
+  constructor(start, newDir) {
+    this.dirs = {
+      N: [-1, 0],
+      S: [1, 0],
+      E: [0, 1],
+      W: [0, -1],
+    };
+    this.current = start;
+    if (!"NESW".includes(newDir))
+      throw new TypeError(`Error in dir specifier: ${newDir}`);
+    this.vector = this.dirs[newDir];
+    this.dir = newDir;
+  }
+  changeDir(newDir) {
+    if (!"NESW".includes(newDir))
+      throw new TypeError(`Error in dir specifier: ${newDir}`);
+    this.vector = this.dirs[newDir];
+    this.dir = newDir;
+  }
+  move() {
+    const [curY, curX] = this.current;
+    const [dirY, dirX] = this.vector;
+    this.current = [curY + dirY, curX + dirX];
+  }
+  get from() {
+    const dirMap = {
+      N: "S",
+      S: "N",
+      E: "W",
+      W: "E",
+    };
+    return dirMap[this.dir];
+  }
+}
+
+class Reflector extends Node {
+  constructor(type) {
+    super();
+    if (type !== "|" && type !== "-")
+      throw new TypeError(`Unknown reflector type: ${type}`);
+    this.type = type;
+    if (type === "|") {
+      this.split = {
+        W: ["N", "S"],
+        E: ["N", "S"],
+      };
+    } else {
+      this.split = {
+        N: ["W", "E"],
+        S: ["W", "E"],
+      };
+    }
+  }
+}
+
+class Mirror extends Node {
+  constructor(type) {
+    super();
+    // "\" === Lmirror
+    // "/" === Rmirror
+    if (type !== "Lmirror" && type !== "Rmirror") {
+      throw new TypeError(`Unknown mirror type: ${type}`);
+    }
+    // this.reflect = {};
+    this.type = type;
+    if (type === "Lmirror") {
+      this._reflect = {
+        N: "E",
+        S: "W",
+        E: "N",
+        W: "S",
+      };
+    } else {
+      this._reflect = {
+        N: "W",
+        S: "E",
+        E: "S",
+        W: "N",
+      };
+    }
+  }
+  reflect(from) {
+    if (!"NESW".includes(from))
+      throw new TypeError(`Error in dir specifier: ${from}`);
+    return this._reflect[from];
+  }
+}
+
+class Empty extends Node {
+  constructor() {
+    super();
+    this.type = "empty";
+  }
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+          .|...\\....
+          |.-.\\.....
+          .....|-...
+          ........|.
+          ..........
+          .........\\
+          ..../.\\\\..
+          .-.-/..|..
+          .|....-|.\\
+          ..//.|....
+        `,
+        expected: 46,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+          .|...\\....
+          |.-.\\.....
+          .....|-...
+          ........|.
+          ..........
+          .........\\
+          ..../.\\\\..
+          .-.-/..|..
+          .|....-|.\\
+          ..//.|....
+        `,
+        expected: 51,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
